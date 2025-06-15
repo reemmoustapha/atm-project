26cf0d384eed6682c15c9c6c0fcdc5440f8c0c3e
reem moustapha <reemmoustapha0@gmail.com>
2025-06-15 22:22:54 +0300

    add while loop and ATM menu with balance 500$

---
+++ atm.py
@@ -0,0 +1,14 @@
+balance = 500
+choice = 0
+
+while choice != 4:
+    print("""
+    welcome to the ATM
+          1. check balance
+          2. deposite money
+          3. withdraw money
+          4. exit
+        """)
+    choice = int(input("your choice: "))
+    
+    
